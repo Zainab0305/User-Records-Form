6c9fd2949f2272edfaf71265cf80bb712fdc87dd
Zainab__ <zinabalhashim595@gmail.com>
2026-07-29 22:18:29 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-function toggleStatus(id) {
-    fetch("in.php?toggle=" + id)
-        .then(response => response.text())
-        .then(newStatus => {
-            document.getElementById("status-" + id).textContent = newStatus;
-        });
-}
